c087c30fb7be1ac9291c1f6a671203646c31ac0b
chaaehyun <connie6301@naver.com>
2024-11-19 19:21:52 +0900

    [FEAT] : db entity와 repository 생성

---
+++ src/main/java/com/korailtalk/server/db/train/entity/Train.java
@@ -0,0 +1,40 @@
+package com.korailtalk.server.db.train.entity;
+
+import com.korailtalk.server.db.timetable.entity.Timetable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 기본 엔티티 스펙
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //같은 패키지 내에서
+@Getter
+@Table(name="trains")
+public class Train {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "bigint", nullable = false) //nullable=flase null값 안됨
+    private Long id;
+
+    @Column(name = "name", columnDefinition = "varchar(255)", nullable = false)
+    private String name;
+
+    @OneToOne(mappedBy ="train", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Timetable timetable;
+
+    //public Train(){
+    //}
+    //@NoArgsConstructor과 같은 것
+    //기본 생성자가 필요한 이유
+    //객체 -> 영속성 -> db
+    //기본 생성자가 영속성을 주기 때문에 필요함
+
+    @Builder
+    public Train(String name, Timetable timetable){
+        this.name = name;
+        this.timetable = timetable;
+    }
+
+}
