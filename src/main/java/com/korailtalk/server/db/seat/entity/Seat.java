558b353d1819c3eb08ea50178934d848afc8d596
chaaehyun <connie6301@naver.com>
2024-11-20 01:32:22 +0900

    [feat] : 단방향 관계로 수정 완료

--- src/main/java/com/korailtalk/server/db/seat/entity/Seat.java
+++ src/main/java/com/korailtalk/server/db/seat/entity/Seat.java
@@ -1,13 +1,11 @@
 package com.korailtalk.server.db.seat.entity;
 
-import com.korailtalk.server.db.ticket.entity.Ticket;
+import com.korailtalk.server.db.coach.entity.Coach;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,6 +17,10 @@ public class Seat {
     @Column(name = "id", columnDefinition = "bigint", nullable = false)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coach_id", columnDefinition = "bigint", nullable = false)
+    private Coach coach;
+
     @Column(name = "name", columnDefinition = "varchar(10)", nullable = false)
     private String name;
 
@@ -28,12 +30,9 @@ public class Seat {
     @Column(name = "seat_sold", columnDefinition = "boolean", nullable = false)
     private boolean seatSold;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Ticket> tickets;
-
     @Builder
-    public Seat(List<Ticket> tickets, String name, Boolean direction, Boolean seatSold){
-        this.tickets = tickets;
+    public Seat(Coach coach, String name, Boolean direction, Boolean seatSold){
+        this.coach = coach;
         this.name = name;
         this.direction = direction;
         this.seatSold = seatSold;
