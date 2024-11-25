2a61a0b2814f8f0c3dce52fc281a6fc69c186deb
chaaehyun <connie6301@naver.com>
2024-11-26 01:56:06 +0900

    [FEAT] : 좌석 선택 API 구현 (충돌 수정)

--- src/main/java/com/korailtalk/server/api/common/enums/ErrorStatus.java
+++ src/main/java/com/korailtalk/server/api/common/enums/ErrorStatus.java
@@ -16,7 +16,8 @@ public enum ErrorStatus {
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "아이디에 해당하는 유저를 찾을 수 없습니다."),
     NOT_FOUND_TICKET(HttpStatus.NOT_FOUND, "아이디에 해당하는 티켓을 찾을 수 없습니다."),
     NOT_FOUND_TRAIN(HttpStatus.NOT_FOUND, "시간표에 해당하는 열차를 찾을 수 없습니다."),
-    NOT_FOUND_TIMETABLE(HttpStatus.NOT_FOUND, "아이디에 해당하는 시간표를 찾을 수 없습니다.");
+    NOT_FOUND_TIMETABLE(HttpStatus.NOT_FOUND, "아이디에 해당하는 시간표를 찾을 수 없습니다."),
+    NOT_FOUND_SEAT(HttpStatus.NOT_FOUND, "아이디에 해당하는 좌석을 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
 
